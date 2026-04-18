815d25b830d12e87f70db934b9bf68a8ea0dc01f
mostafa3122 <moustafahelal92@gmail.com>
2026-04-18 14:10:03 +0200

    setup project and organize folder stucture

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-capitalize' >food app</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/modules/Recipes/components/RecipesData/RecipesData.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RecipesData() {
+    return (
+        <div>
+            RecipesData
+        </div>
+    )
+}
